f604b8b190dfa0d7b08ddb340ba9aadb5fe835b6
Daiwei <daiweilu123@gmail.com>
2016-06-21 11:47:09 -0400

    Add method to fetch user profile

--- src/index.js
+++ src/index.js
@@ -47,10 +47,48 @@ function createClient(opts) {
     return access_token;
   });
 
+  // { login: 'd6u',
+  //   id: 1234,
+  //   avatar_url: 'https://avatars.githubusercontent.com/xxxxxxx',
+  //   gravatar_id: '',
+  //   url: 'https://api.github.com/users/xyz',
+  //   html_url: 'https://github.com/xyz',
+  //   followers_url: 'https://api.github.com/users/xyz/followers',
+  //   following_url: 'https://api.github.com/users/xyz/following{/other_user}',
+  //   gists_url: 'https://api.github.com/users/xyz/gists{/gist_id}',
+  //   starred_url: 'https://api.github.com/users/xyz/starred{/owner}{/repo}',
+  //   subscriptions_url: 'https://api.github.com/users/xyz/subscriptions',
+  //   organizations_url: 'https://api.github.com/users/xyz/orgs',
+  //   repos_url: 'https://api.github.com/users/xyz/repos',
+  //   events_url: 'https://api.github.com/users/xyz/events{/privacy}',
+  //   received_events_url: 'https://api.github.com/users/xyz/received_events',
+  //   type: 'User',
+  //   site_admin: false,
+  //   name: 'XY Z',
+  //   company: null,
+  //   blog: 'http://xxx',
+  //   location: null,
+  //   email: 'xxx@email.com',
+  //   hireable: true,
+  //   bio: null,
+  //   public_repos: 123,
+  //   public_gists: 123,
+  //   followers: 123,
+  //   following: 123,
+  //   created_at: '2012-08-14T22:42:49Z',
+  //   updated_at: '2016-02-20T03:02:44Z' }
+
+  const fetchUserProfile = wrap(function *(access_token) {
+    const client = github.client(access_token);
+    const [, profile] = yield client.getAsync('/user');
+    return profile;
+  });
+
   return {
     client: github,
     createLoginUrl,
     handleLoginCallback,
+    fetchUserProfile,
   };
 }
 
